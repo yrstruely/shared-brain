246a1046e6211776be4dc4773b678abb598595ef
Kerry Harris <nzfluentit@gmail.com>
2026-07-11 20:54:10 +1200

    fix: remove vault-side code files (belong in external directory)

--- projects/hello-world/backend/src/welcome/welcome.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { WelcomeResponseDto } from './dto/welcome-response.dto';
-
-@Injectable()
-export class WelcomeService {
-  getWelcomeMessage(): WelcomeResponseDto {
-    return new WelcomeResponseDto('Hello, World!');
-  }
-}
